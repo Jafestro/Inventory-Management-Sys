670dc13b96819adfe2aa2258352b85a9749812dd
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-08-30 10:50:36 +0300

    chore: Update repository interfaces to use Integer instead of Long for entity IDs

--- src/main/java/com/reppuhallinta/inventory_management_sys/repository/ProductRepository.java
+++ src/main/java/com/reppuhallinta/inventory_management_sys/repository/ProductRepository.java
@@ -4,10 +4,8 @@ import com.reppuhallinta.inventory_management_sys.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Products, Long> {
+public interface ProductRepository extends JpaRepository<Products, Integer> {
 
-    Optional<Products> findById(int id);
 }
--- src/main/java/com/reppuhallinta/inventory_management_sys/repository/UserRepository.java
+++ src/main/java/com/reppuhallinta/inventory_management_sys/repository/UserRepository.java
@@ -1,5 +1,4 @@
 package com.reppuhallinta.inventory_management_sys.repository;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,8 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.reppuhallinta.inventory_management_sys.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     
-    Optional<User> findById(int id);
 
 }

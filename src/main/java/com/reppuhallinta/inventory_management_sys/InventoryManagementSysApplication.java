a69a774c4cd02f60280f2329cd06d3a672c44cd2
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-09-11 18:17:37 +0300

    Add basic UI (not working)

--- src/main/java/com/reppuhallinta/inventory_management_sys/InventoryManagementSysApplication.java
+++ src/main/java/com/reppuhallinta/inventory_management_sys/InventoryManagementSysApplication.java
@@ -1,13 +1,47 @@
 package com.reppuhallinta.inventory_management_sys;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class InventoryManagementSysApplication {
+public class InventoryManagementSysApplication extends Application {
+
+	private ConfigurableApplicationContext springContext;
+
+	@Override
+	public void init() {
+		springContext = SpringApplication.run(InventoryManagementSysApplication.class);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/reppuhallinta/inventory_management_sys/view/Products.fxml"));
+		fxmlLoader.setControllerFactory(springContext::getBean);
+		Scene scene = new Scene(fxmlLoader.load());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Inventory Management System");
+		primaryStage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		springContext.stop();
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagementSysApplication.class, args);
+		launch(args);
 	}
+
+	/*public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSysApplication.class, args);
+	}*/
 
 }

24dad2f17f21c7d1c60ee623c2c0384da1851bec
jonnekoi <jonnekoivisto00@gmail.com>
2024-09-23 09:45:18 +0300

    label

--- src/main/java/com/reppuhallinta/inventory_management_sys/controller/ProductViewController.java
+++ src/main/java/com/reppuhallinta/inventory_management_sys/controller/ProductViewController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import javafx.scene.control.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -18,12 +19,7 @@ import com.reppuhallinta.inventory_management_sys.utils.FXMLLoaderUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import session.CustomSessionManager;
